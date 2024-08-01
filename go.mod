c46e948c126e29294cb086cb6dccaed48160adf5
vipul-rawat <vipul.rawat@gofr.dev>
2024-08-01 17:46:07 +0530

    add tests for dockerize function

--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module kops.dev
 
 go 1.22
 
-require gofr.dev v1.15.0
+require (
+	github.com/stretchr/testify v1.9.0
+	gofr.dev v1.15.0
+)
 
 require (
 	cloud.google.com/go v0.115.0 // indirect
@@ -17,6 +20,7 @@ require (
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cenkalti/backoff/v4 v4.3.0 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/dustin/go-humanize v1.0.1 // indirect
 	github.com/eclipse/paho.mqtt.golang v1.4.3 // indirect
@@ -46,6 +50,7 @@ require (
 	github.com/openzipkin/zipkin-go v0.4.3 // indirect
 	github.com/pierrec/lz4/v4 v4.1.21 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/prometheus/client_golang v1.19.1 // indirect
 	github.com/prometheus/client_model v0.6.1 // indirect
 	github.com/prometheus/common v0.55.0 // indirect
@@ -84,6 +89,7 @@ require (
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20240708141625-4ad9e859172b // indirect
 	google.golang.org/grpc v1.65.0 // indirect
 	google.golang.org/protobuf v1.34.2 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 	modernc.org/gc/v3 v3.0.0-20240107210532-573471604cb6 // indirect
 	modernc.org/libc v1.52.1 // indirect
 	modernc.org/mathutil v1.6.0 // indirect
---
+++ internal/file/docker_test.go
@@ -0,0 +1,108 @@
+package file
+
+import (
+	"io"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"gofr.dev/pkg/gofr"
+	"gofr.dev/pkg/gofr/container"
+	"gofr.dev/pkg/gofr/logging"
+)
+
+func Test_CreateDockerFile(t *testing.T) {
+	var (
+		goContent = `FROM alpine:latest
+RUN apk add --no-cache tzdata ca-certificates
+COPY main ./main
+RUN chmod +x /main
+EXPOSE 8080
+CMD ["/main"]`
+		jsContent = `FROM node:10-alpine
+RUN mkdir -p /home/node/app/node_modules && chown -R node:node /home/node/app
+WORKDIR /home/node/app
+
+COPY package*.json ./
+USER node
+RUN npm install
+COPY --chown=node:node . .
+
+EXPOSE 8080
+CMD [ "node", "app.js" ]`
+		javaContent = `FROM openjdk
+
+COPY app.jar app.jar
+
+# copy project dependencies
+ADD lib/ lib/
+
+ENTRYPOINT ["java","-jar","app.jar"]`
+	)
+
+	defer os.Remove("Dockerfile")
+
+	testCases := []struct {
+		desc    string
+		lang    string
+		expErr  error
+		content string
+	}{
+		{
+			desc:   "language not supported yet",
+			lang:   "pascal",
+			expErr: errLanguageNotSupported,
+		},
+		{
+			desc:    "golang DockerFile",
+			lang:    golang,
+			expErr:  nil,
+			content: goContent,
+		},
+		{
+			desc:    "javascript DockerFile",
+			lang:    js,
+			expErr:  nil,
+			content: jsContent,
+		},
+		{
+			desc:    "java DockerFile",
+			lang:    java,
+			expErr:  nil,
+			content: javaContent,
+		},
+	}
+
+	ctx := &gofr.Context{
+		Container: &container.Container{Logger: logging.NewMockLogger(logging.INFO)},
+	}
+
+	for _, tc := range testCases {
+		err := CreateDockerFile(ctx, tc.lang, "8080")
+
+		assert.Equal(t, tc.expErr, err)
+
+		if err == nil {
+			testDockerFile(t, tc.content)
+		}
+	}
+}
+
+func testDockerFile(t *testing.T, expected string) {
+	t.Helper()
+
+	_, err := os.Stat("DockerFile")
+	if err != nil {
+		t.Errorf("Test failed, could not create DockerFile, error : %v", err)
+	}
+
+	file, err := os.Open("DockerFile")
+	if err != nil {
+		t.Errorf("Test failed, error reading DockerFile, error : %v", err)
+	}
+
+	b, err := io.ReadAll(file)
+
+	assert.Equal(t, expected, string(b))
+}
